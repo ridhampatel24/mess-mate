98fbdecd472607ff758a2c7eaf9f819e2b8236ef
Ridham Patel <ridhampatel2002@gmail.com>
2023-04-01 14:02:52 +0530

    new

--- conveter.php
+++ conveter.php
@@ -10,7 +10,7 @@ if ($_SESSION['user_type'] != 'admin') {
 $sql = "SELECT * FROM meal";
 $result = mysqli_query($conn, $sql);
 $file = fopen("data.csv", "w");
-fputcsv($file, array("Date", "Lunch/Dinner","users","user count","Menu","Revenu"));
+fputcsv($file, array("Date", "Lunch/Dinner","users","user count","Menu","Revenu","Wastage Plate","Wastage Amount"));
 
 
 while ($row = mysqli_fetch_assoc($result)) {
@@ -19,9 +19,15 @@ while ($row = mysqli_fetch_assoc($result)) {
     } else {
         $row['type'] = 'Dinner';
     }
+
+    if($row['items'] != NULL){
+      $items = explode('||', $row['items']);
+    }
+    $items = implode(',', $items);
+    $row['items'] = $items;
     $cnt = sizeof(explode(',', $row['users']));
     $revenu  = $row['price'] * $cnt;
-    fputcsv($file, array($row['date'], $row['type'], $row['users'],$cnt,$row['items'], $revenu));
+    fputcsv($file, array($row['date'], $row['type'], $row['users'],$cnt,$row['items'], $revenu,$row['wastage_user'],$row['wastage_user']*$row['price']));
 }
 
 $file = "data.csv";
